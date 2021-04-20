64acc79cd3e047154e01bd08bc25a23f17eede14
newave986 <kingofthefall538@gmail.com>
2021-04-20 17:46:06 +0900

    Programmers #67256 키패드 누르기
    Ldis, Rdis 구하는 과정 간소화
    
    쓸데없는 if문 삭제

--- Java/PROGRAMMERS/210419_67256_키패드 누르기.java
+++ Java/PROGRAMMERS/210419_67256_키패드 누르기.java
@@ -1,39 +1,24 @@
 class Solution {
 
-    public static int Lstate = -1, Rstate = -1;
-    public static String answer = "";
+    public int Lstate = 10, Rstate = 11;
+    public String answer = "";
 
-    public static boolean isItLeft(String hand, int n) {
+    public boolean isItLeft(String hand, int n) {
 
-        int[] firstIndex = {3, 0, 0, 0, 1, 1, 1, 2, 2, 2};
-        int[] secondIndex = {1, 0, 1, 2, 0, 1, 2, 0, 1, 2};
-        
-        int Ldis = 0, Rdis = 0; 
-        
-        if (Lstate == -1) { 
-            Ldis = Math.abs(3 - firstIndex[n]) + Math.abs(0 - secondIndex[n]);
-            Rdis = Math.abs(firstIndex[Rstate] - firstIndex[n]) + Math.abs(secondIndex[Rstate] - secondIndex[n]);
-        }
-        
-        else if (Rstate == -1) { 
-            Ldis = Math.abs(firstIndex[Lstate] - firstIndex[n]) + Math.abs(secondIndex[Lstate] - secondIndex[n]);
-            Rdis = Math.abs(3 - firstIndex[n]) + Math.abs(2 - secondIndex[n]); 
-        }
+        int[] firstIndex = {3, 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3};
+        int[] secondIndex = {1, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 2};
     
-        else {
-            Ldis = Math.abs(firstIndex[Lstate] - firstIndex[n]) + Math.abs(secondIndex[Lstate] - secondIndex[n]);
-            Rdis = Math.abs(firstIndex[Rstate] - firstIndex[n]) + Math.abs(secondIndex[Rstate] - secondIndex[n]);
-        }
+        int Ldis = Math.abs(firstIndex[Lstate] - firstIndex[n]) + Math.abs(secondIndex[Lstate] - secondIndex[n]);
+        int Rdis = Math.abs(firstIndex[Rstate] - firstIndex[n]) + Math.abs(secondIndex[Rstate] - secondIndex[n]);
         
         if (Ldis == Rdis) {
             if (hand.equals("left")) return true;
-            else return false;
-            }
+        }
         else if (Ldis < Rdis) return true;
-        else return false;
+        return false;
     }
 
-    public static void handResult(String hand, int n) {
+    public void handResult(String hand, int n) {
         if (hand.equals("left")) {
             answer += "L";
             Lstate = n;
@@ -44,7 +29,7 @@ class Solution {
         }
     }
 
-    public static String solution(int[] numbers, String hand) {
+    public String solution(int[] numbers, String hand) {
 
         for (int i = 0; i < numbers.length; i++) {
 
@@ -52,17 +37,13 @@ class Solution {
             else if (numbers[i] == 3 || numbers[i] == 6 || numbers[i] == 9) handResult("right", numbers[i]);
 
             else {
-                if (i == 0) {
-                    if (hand.equals("left")) handResult("left", numbers[i]);
-                    else handResult("right", numbers[i]);
-                }
-                else {
-                    if (isItLeft(hand, numbers[i])) handResult("left", numbers[i]);
-                    else handResult("right", numbers[i]);
-                }
+                if (isItLeft(hand, numbers[i])) handResult("left", numbers[i]);
+                else handResult("right", numbers[i]);
             }
-
+            
         }
+        
         return answer;
+        
     }
 }
